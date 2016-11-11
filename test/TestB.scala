2e8293c9976f7464e4e13c1769a77a5ec53e09c5
Caleb Carvalho <you@example.com>
2016-11-11 09:24:44 +0100

    added bpm history checks

--- test/TestB.scala
+++ test/TestB.scala
@@ -10,6 +10,7 @@ import java.util
 
 import MyActivitiEngine._
 import org.activiti.engine.form.FormProperty
+import org.activiti.engine.history.{HistoricDetail, HistoricFormProperty}
 
 import scala.collection.JavaConversions._
 
@@ -34,15 +35,32 @@ class TestB extends RuntimeProcess {
     formData.put("numberOfDays", "2")
     formData.put("startDate", "20-06-2017")
     formData.put("vacationMotivation", "R&R")
-
     formData.put("vacationApproved", "true")
     formService.submitTaskFormData(task.getId, formData)
-
+    val h = new java.util.ArrayList[FormProperty]()
 
 
   }
 
   "task name " should "be" in {
+
+//    val historyData = new java.util.ArrayList[HistoricDetailQuery]()
+
+   val historyData:util.List[HistoricDetail] = historyService.createHistoricDetailQuery().formProperties().list()
+
+    for(v<-historyData){
+//      println(v.getActivityInstanceId, v.getExecutionId, v.getId, v.getProcessInstanceId, v.getTaskId, v.getTime)
+        println(v.asInstanceOf[HistoricFormProperty].getPropertyId,
+          v.asInstanceOf[HistoricFormProperty].getPropertyValue)
+    }
+
+//    var formProperty = historyData.get(0).asInstanceOf[HistoricFormProperty]
+//
+//    println(formProperty.getPropertyId, formProperty.getPropertyValue)
+//    formProperty = historyData.get(1).asInstanceOf[HistoricFormProperty]
+//    println(formProperty.getPropertyId, formProperty.getPropertyValue)
+//
+
     repoService.createDeployment().addClasspathResource("VacationRequest.bpmn20.xml").deploy()
     val pi = runtimeService.startProcessInstanceByKey("vacationRequest")
     val task = taskService.createTaskQuery().singleResult()
