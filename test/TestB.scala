c3661df8d74a1f02d57f29874eac8266aeae7a41
Caleb Carvalho <you@example.com>
2016-11-11 10:58:37 +0100

    remove commnents

--- test/TestB.scala
+++ test/TestB.scala
@@ -25,44 +25,32 @@ class TestB extends RuntimeProcess {
     val task = taskService.createTaskQuery().singleResult()
     println(task)
 
-    val xs = new java.util.ArrayList[FormProperty]()
-    xs.addAll(formService.getStartFormData(pi.getProcessDefinitionId).getFormProperties)
+    val x: util.List[FormProperty] = formService.getStartFormData(pi.getProcessDefinitionId).getFormProperties
 
-    for(xx<-xs){
-      println(xx.getId, xx.getName, xx.getValue, xx.getType.getName)
+    for(xx<-x){
+      println(xx.getId, xx.getName)
     }
+
     val formData = new util.HashMap[String, String]()
     formData.put("numberOfDays", "2")
     formData.put("startDate", "20-06-2017")
     formData.put("vacationMotivation", "R&R")
     formData.put("vacationApproved", "true")
     formService.submitTaskFormData(task.getId, formData)
-    val h = new java.util.ArrayList[FormProperty]()
-
 
   }
 
   "task name " should "be" in {
 
-//    val historyData = new java.util.ArrayList[HistoricDetailQuery]()
+    val historyData: util.List[HistoricDetail] = historyService.createHistoricDetailQuery().formProperties().list()
 
-   val historyData:util.List[HistoricDetail] = historyService.createHistoricDetailQuery().formProperties().list()
-
-    for(v<-historyData){
-//      println(v.getActivityInstanceId, v.getExecutionId, v.getId, v.getProcessInstanceId, v.getTaskId, v.getTime)
-        println(v.asInstanceOf[HistoricFormProperty].getPropertyId,
-          v.asInstanceOf[HistoricFormProperty].getPropertyValue)
+    for (v <- historyData) {
+      println(v.asInstanceOf[HistoricFormProperty].getPropertyId,
+        v.asInstanceOf[HistoricFormProperty].getPropertyValue)
     }
 
-//    var formProperty = historyData.get(0).asInstanceOf[HistoricFormProperty]
-//
-//    println(formProperty.getPropertyId, formProperty.getPropertyValue)
-//    formProperty = historyData.get(1).asInstanceOf[HistoricFormProperty]
-//    println(formProperty.getPropertyId, formProperty.getPropertyValue)
-//
-
     repoService.createDeployment().addClasspathResource("VacationRequest.bpmn20.xml").deploy()
-    val pi = runtimeService.startProcessInstanceByKey("vacationRequest")
+    runtimeService.startProcessInstanceByKey("vacationRequest")
     val task = taskService.createTaskQuery().singleResult()
     println(task)
   }
